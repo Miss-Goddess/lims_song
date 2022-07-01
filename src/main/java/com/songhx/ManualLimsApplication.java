cc2ebe3c1f5415ea0548950c998ed039d5a9fef5
Miss-Goddess <1606494355@qq.com>
2022-07-02 00:28:21 +0800

    Initial commit

---
+++ src/main/java/com/songhx/ManualLimsApplication.java
@@ -0,0 +1,11 @@
+package com.songhx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 标记该类为主程序启动类
+public class ManualLimsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManualLimsApplication.class,args);
+    }
+}
